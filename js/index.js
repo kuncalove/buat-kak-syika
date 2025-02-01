6f7ea69d9b94479f55d6153b94ddf1944e38af47
kuncalove <75157301+kuncalove@users.noreply.github.com>
2025-02-01 23:17:42 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hai kak syika 😸👋'.split('')
+const text = 'Hai kak syika'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
